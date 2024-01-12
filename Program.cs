6bcb76a985e72a9d95a6480852e05acd05d355b9
dpech <dpech@DANNA>
2024-01-12 12:08:59 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace GAGitHubBasics_dpech
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// Start of Main Code Block
+            // Code goes inside of here
+            Console.WriteLine("Hello, Danna!");
+
+            // Console keyword that access actions with the Console
+            
+            Console.WriteLine("Cloud Networking");
+        }// End of Main Code Block
+    }
+}
